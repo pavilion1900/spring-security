7e4c43321272cc2b2f78599f52b01451cc1edc3b
Korolenko Sergey <pavilion1900@gmail.com>
2022-12-11 20:05:56 +0300

    84 Шифрование

--- src/main/java/ru/clevertec/configuration/SecurityConfiguration.java
+++ src/main/java/ru/clevertec/configuration/SecurityConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.clevertec.service.PersonDetailsService;
 
@@ -18,7 +18,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailsService);
+        auth.userDetailsService(personDetailsService)
+                .passwordEncoder(getPasswordEncoder());
     }
 
     @Override
@@ -40,6 +41,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/ru/clevertec/service/RegistrationService.java
+++ src/main/java/ru/clevertec/service/RegistrationService.java
@@ -1,6 +1,7 @@
 package ru.clevertec.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.clevertec.entity.Person;
@@ -11,9 +12,12 @@ import ru.clevertec.repository.PersonRepository;
 public class RegistrationService {
 
     private final PersonRepository personRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public void register(Person person) {
+        String encodedPassword = passwordEncoder.encode(person.getPassword());
+        person.setPassword(encodedPassword);
         personRepository.save(person);
     }
 }
