86ca87e08a19898d9b60cce7089bbba6605db27a
Korolenko Sergey <pavilion1900@gmail.com>
2022-12-11 20:42:32 +0300

    85 CSRF

--- src/main/java/ru/clevertec/configuration/SecurityConfiguration.java
+++ src/main/java/ru/clevertec/configuration/SecurityConfiguration.java
@@ -24,8 +24,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/auth/login", "/auth/registration", "error").permitAll()
                 .anyRequest().authenticated()
                 .and()
